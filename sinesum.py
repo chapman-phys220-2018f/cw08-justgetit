29d65c84d974ecdd3e5fc4af08ab7822144c62fa
GageKizzar <gagekizzar@gmail.com>
2018-10-23 18:15:35 +0000

    Just some notebooks stuff to do left

--- sinesum.py
+++ sinesum.py
@@ -1,38 +1,63 @@
 #!/usr/bin/env python3
 
 import numpy as np
+import matplotlib.pyplot as plt
 
-def s(t,n,T=(2*np.pi)):
+
+def s(t, n, T=(2 * np.pi)):
     """s(t,n,T)
 
     """
-    k = 1
-    added = 0
-    sum1 = 0
-    list1 = []
-    while True:
-        added = (1/((2*k)-1)) * np.sin(((2*(2*k)-1)*np.pi * t)/T)
-        sum1 = sum1 + added
-        list1.append(sum1)
-        if (k == n):
-            break
-        k += 1
-    return ((4/np.pi)*sum1)
+    k = np.arange(1, n + 1)
 
-def f(t,T=(2*np.pi)):
+    s = (1 / ((2 * k) - 1)) * np.sin((2 * ((2 * k) - 1) * np.pi * t) / T)
+    return (4 / np.pi) * np.sum(s)
+
+
+def splot(t, n, T=(2 * np.pi)):
+    """splot(t,n,T)
+
+    """
+    k = np.arange(1, n + 1)
+
+    s = (4 / np.pi) * (1 / ((2 * k) - 1)) * np.sin((2 * ((2 * k) - 1) * np.pi * t) / T)
+
+    S = []
+    for i in k:
+        v = 0
+        tot = 0
+        while v < i - 1:
+            tot = tot + s[v]
+            v += 1
+        S.append(tot)
+
+    return S
+
+
+def f(t, T=(2 * np.pi)):
     """f(t,T)
     t = test variable
     T = domain demonstrating variable
 
     returns 1,0 or -1 depending on t's relation to T
     """
-    if (t > 0) and (t < T/2):
+    if (t > 0) and (t <= T / 2):
         return 1
-    elif (t < 0) and (t > -T/2):
+    elif (t < 0) and (t >= -T / 2):
         return -1
     elif (t == 0):
         return 0
     else:
         print('t is not in the domain, please try again.')
 
-print(s((np.pi),1000,(2*np.pi)))
+
+def plot(t, n, T=(2 * np.pi)):
+    """
+    """
+    S = splot(t, n, T)
+
+    plt.grid()
+
+    plt.xlim(0, n)
+
+    return plt.plot(S)
